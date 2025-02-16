6c308f7ae942e266573a44d2d20855e7afe0eb45
Rafael Pambo <rpambo@c1r6s6.42luanda.com>
2025-02-16 10:22:20 +0100

    all

--- 30-days-of-code/day-1/data-structur.cpp
+++ 30-days-of-code/day-1/data-structur.cpp
@@ -1,4 +1,21 @@
-int main ()
-{
+#include <iostream>
+#include <iomanip>
+#include <limits>
 
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+    int f;
+    double m;
+    string r;
+    
+    cin >> f;
+    cin >> m;
+    cin.ignore();
+    getline(cin, r);
+    cout << f + i << endl << fixed << setprecision(1) << d + m << endl << s + r;
+    return 0;
 }
